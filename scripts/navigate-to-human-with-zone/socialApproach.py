7fb3bc1cb26a31249f3fdd3848f2d721959b87bb
LemonPepperSeasoning <tish386@aucklanduni.ac.nz>
2021-02-11 15:22:20 +1300

    Added social approach node : - Test 1:

--- scripts/navigate-to-human-with-zone/socialApproach.py
+++ scripts/navigate-to-human-with-zone/socialApproach.py
@@ -2,14 +2,20 @@
 import rospy
 import rospkg
 
+from body_tracker_msgs.msg import BodytTracker
+from message_filters import ApproximateTimeSynchronizer, Subscriber
+
+def gotimage( input1, input2 , input3, input4):
+    print ("INPUT : {}, {}, {}, {}".format(input1,input2,input3,input4))
+    
+
 if __name__ == "__main__" :
-    rospy.init_node('socialApproach')
+    rospy.init_node('socialApproach')    
 
-    rospy.Subscriber("/tone_w/where" , MultiPersons, queue_size=1)
-    rospy.Subscriber("/tone_w/where" , MultiPersons, queue_size=1)
-    rospy.Subscriber("/tone_w/where" , MultiPersons, queue_size=1)
-    rospy.Subscriber("/tone_w/where" , MultiPersons, queue_size=1)
+    sub1 = Subscriber("/realsense1/body_tracker/position", BodytTracker)
+    sub2 = Subscriber("/realsense2/body_tracker/position", BodytTracker)
+    sub3 = Subscriber("/realsense3/body_tracker/position", BodytTracker)
+    sub4 = Subscriber("/realsense4/body_tracker/position", BodytTracker)
 
-    Direction = "NORTH"
-    
-    
+    ats = ApproximateTimeSynchronizer([sub1, sub2, sub3, sub4], queue_size=5, slop=0.05)
+    ats.registerCallback(gotimage)
